8a89cf1f61683a1ec5d5a8d9b4836a70d6ccb362
Marcela de souza <118936030+mardesouza@users.noreply.github.com>
2023-11-14 19:41:26 -0300

    Add files via upload

---
+++ styles/script.js
@@ -0,0 +1,12 @@
+let header = document.getElementById('header')
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY >=200) {
+        header.style.background ='#191919'
+    } else {
+        header.style.background = 'transparent'
+    }
+
+} )
+
+
